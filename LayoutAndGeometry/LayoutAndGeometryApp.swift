3ee2cde6684cd08797ed1f6bb6ed9cc4f13f9fda
Faustharus <chailloleau-damien@outlook.fr>
2024-08-01 21:36:20 +0200

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Damien Chailloleau on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
