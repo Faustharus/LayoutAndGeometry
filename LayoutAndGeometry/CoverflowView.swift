2c4e28a0585ee76d9b7b2cfa9e5632b7d2c5afab
Faustharus <chailloleau-damien@outlook.fr>
2024-08-05 17:34:14 +0200

    Exercice Project Done

--- LayoutAndGeometry/CoverflowView.swift
+++ LayoutAndGeometry/CoverflowView.swift
@@ -9,23 +9,44 @@ import SwiftUI
 
 struct CoverflowView: View {
     var body: some View {
+        // MARK: Coverflow Effect with GeometryReader
+//        ScrollView(.horizontal, showsIndicators: false) {
+//            HStack(spacing: 0) {
+//                ForEach(1 ..< 20) { number in
+//                    GeometryReader { proxy in
+//                        Text("Number \(number)")
+//                            .font(.title)
+//                            .padding()
+//                            .background(.red)
+//                            .rotation3DEffect(
+//                                .degrees(-proxy.frame(in: .global).minX / 8),
+//                                axis: (x: 0, y: 1, z: 0)
+//                            )
+//                            .frame(width: 200, height: 200)
+//                    }
+//                    .frame(width: 200, height: 200)
+//                }
+//            }
+//        }
+        
+        // MARK: Coverflow Effect with .visualEffect() modifier
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
-                ForEach(0 ..< 20) { number in
-                    GeometryReader { proxy in
-                        Text("Number \(number)")
-                            .font(.title)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(
-                                .degrees(-proxy.frame(in: .global).minX / 8),
-                                axis: (x: 0, y: 1, z: 0)
-                            )
-                    }
-                    .frame(width: 200, height: 200)
+                ForEach(1 ..< 20) { number in
+                    Text("Number \(number)")
+                        .font(.title)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                            content
+                                .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX / 8), axis: (x: 0.0, y: 1.0, z: 0.0))
+                        }
                 }
             }
+            .scrollTargetLayout()
         }
+        .scrollTargetBehavior(.viewAligned)
     }
 }
 
--- LayoutAndGeometry/ScrollViewEffectHelix.swift
+++ LayoutAndGeometry/ScrollViewEffectHelix.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 struct ScrollViewEffectHelix: View {
     
-    let colors: [Color] = [.red, .blue, .green, .yellow, .purple, .cyan, .orange, .mint, .indigo, .teal]
+    let colors: [Color] = [.red, .blue, .green, .yellow, .purple, .pink, .orange]
     
     var body: some View {
+        // MARK: Complex Helix Revamp - Exercice
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0 ..< 60) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(hue: min(1,proxy.frame(in: .global).minY / fullView.size.height), saturation: 1, brightness: 1))
+                            .rotation3DEffect(
+                                .degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 4.5,
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                            .opacity(proxy.frame(in: .global).minY / 200)
+                            .scaleEffect(max(0.5, proxy.frame(in: .global).minY / 400))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+        
         // MARK: Complex Helix
 //        GeometryReader { fullView in
 //            ScrollView {
@@ -20,7 +41,7 @@ struct ScrollViewEffectHelix: View {
 //                        Text("Row #\(index)")
 //                            .font(.title)
 //                            .frame(maxWidth: .infinity)
-//                            .background(colors[index % 10])
+//                            .background(colors[index % colors.count])
 //                            .rotation3DEffect(
 //                                .degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 7,
 //                                axis: (x: 0.0, y: 1.0, z: 0.0)
@@ -32,21 +53,21 @@ struct ScrollViewEffectHelix: View {
 //        }
         
         // MARK: Simple Helix
-        ScrollView {
-            ForEach(0 ..< 60) { index in
-                GeometryReader { proxy in
-                    Text("Row #\(index)")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                        .background(colors[index % 10])
-                        .rotation3DEffect(
-                            .degrees(proxy.frame(in: .global).minY / 7),
-                            axis: (x: 0.0, y: 1.0, z: 0.0)
-                        )
-                }
-                .frame(height: 40)
-            }
-        }
+//        ScrollView {
+//            ForEach(0 ..< 60) { index in
+//                GeometryReader { proxy in
+//                    Text("Row #\(index)")
+//                        .font(.title)
+//                        .frame(maxWidth: .infinity)
+//                        .background(colors[index % colors.count])
+//                        .rotation3DEffect(
+//                            .degrees(proxy.frame(in: .global).minY / 7),
+//                            axis: (x: 0.0, y: 1.0, z: 0.0)
+//                        )
+//                }
+//                .frame(height: 40)
+//            }
+//        }
     }
 }
 
